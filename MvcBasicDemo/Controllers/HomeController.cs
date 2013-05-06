f91aed6f2d6cdb403dde683819926038187b7d0f
dkroy <dylan.k.roy@gmail.com>
2013-05-06 04:58:28 -0500

    Initial commit, before cleanup and public tut.

---
+++ MvcBasicDemo/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcApplication1.Models;
+
+namespace MvcApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your app description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+            ViewBag.Title = "Contact";
+            List<Contact> contacts = LoadContacts();
+            return View(contacts);
+        }
+
+        private List<Contact> LoadContacts()
+        {
+            List<Contact> contacts = new List<Contact>();
+            for (int i = 0; i < 10; i++ )
+            {
+                Contact NewContact = new Contact();
+                NewContact.name = "name " + i;
+                NewContact.phone = "phone " + i;
+                NewContact.email = "email " + i;
+                contacts.Add(NewContact);
+            }
+            return contacts;
+        }
+
+    }
+}
---
+++ MvcBasicDemo/Models/Contact.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication1.Models
+{
+    public class Contact
+    {
+        public string name { get; set; }
+        public string phone { get; set; }
+        public string email { get; set; }
+    }
+}
